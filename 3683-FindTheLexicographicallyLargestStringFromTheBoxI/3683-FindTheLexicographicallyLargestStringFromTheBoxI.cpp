6acbfe6913fb5a83cfdf514228b87d14c879e85e
Gagan Sharma <158063668+gagan1146@users.noreply.github.com>
2025-12-19 19:26:48 +0530

    Create 3683-FindTheLexicographicallyLargestStringFromTheBoxI

---
+++ 3683-FindTheLexicographicallyLargestStringFromTheBoxI/3683-FindTheLexicographicallyLargestStringFromTheBoxI.cpp
@@ -0,0 +1,12 @@
+// Last updated: 19/12/2025, 19:22:46
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if(numFriends==1) return word;
+        string res = "";
+        for(int i=0;i<word.size();i++){
+            res = max( res, word.substr(i,word.length() - numFriends + 1 ));
+        }
+        return res;
+    }
+};
