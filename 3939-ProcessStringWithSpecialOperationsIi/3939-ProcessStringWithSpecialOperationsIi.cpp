156e6c1bfe1e5612c63429f5df75a6d4b6cbcc99
Gagan Sharma <158063668+gagan1146@users.noreply.github.com>
2025-12-19 19:25:14 +0530

    Create 3939-ProcessStringWithSpecialOperationsIi

---
+++ 3939-ProcessStringWithSpecialOperationsIi/3939-ProcessStringWithSpecialOperationsIi.cpp
@@ -0,0 +1,29 @@
+// Last updated: 19/12/2025, 19:21:12
+class Solution {
+public:
+    using ll = long long;
+    char processStr(string s, long long k) {
+        ll n=0;
+        for(auto &x: s){
+            if(x=='*') n = max(0ll,n-1);
+            else if(x=='#') n*=2;
+            else if(x=='%') continue;
+            else n++;
+        }
+        if(n<=k) return '.';
+        reverse(s.begin(),s.end());
+        for(auto &x:s){
+            if(x=='*') n++;
+            else if(x=='#'){
+                n/=2;
+                if(k>=n) k-=n;
+            }
+            else if(x=='%') k=n-k-1;
+            else{
+                n--;
+                if(n<=k) return x;
+            }
+        }
+        return '.';
+    }
+};
