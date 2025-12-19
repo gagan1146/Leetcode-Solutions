dc45cdb5566309fd3b00e33abdd90f7a13afbea8
Gagan Sharma <158063668+gagan1146@users.noreply.github.com>
2025-12-19 19:26:23 +0530

    Create 3755-MaximumProductOfFirstAndLastElementsOfASubsequence

---
+++ 3755-MaximumProductOfFirstAndLastElementsOfASubsequence/3755-MaximumProductOfFirstAndLastElementsOfASubsequence.cpp
@@ -0,0 +1,13 @@
+// Last updated: 19/12/2025, 19:22:21
+class Solution {
+public:
+    long long maximumProduct(vector<int>& A, int m) {
+        long long ma = A[0], mi = A[0], res = 1LL * A[0] * A[m - 1];
+        for (int i = m - 1; i < A.size(); ++i) {
+            ma = fmax(ma, A[i - m + 1]);
+            mi = fmin(mi, A[i - m + 1]);
+            res = max({res, mi * A[i], ma * A[i]});
+        }
+        return res;
+    }
+};
